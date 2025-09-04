8eca90bc0e3ffef3c7fe865d235bdf16692ff018
Angela Castro <andycastro05@gmail.com>
2025-09-03 22:19:10 -0600

    Implementation of the algorithms seen in class

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -7,33 +7,128 @@
 
 namespace SortingAlgorithms {
 
+    //Funcion de swap auxiliar para distintos algoritmos
+void swap(int arr[], int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
 void bubbleSort(int arr[], int n) {
-    
+    for (int i = 0; i < n-1; i++) {
+        bool swapped = false; //Checa internamente si ya esta acomodado
+        
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr, j, j+1);
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) {
+            break;
+        }
+    }
 }
 
 void selectionSort(int arr[], int n) {
-    
+    for (int i = 0; i < n-1; i++) {
+        int minIndex = i;
+        
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        if (minIndex != i) {
+            swap(arr, i, minIndex);
+        }
+    }
 }
 
 void insertionSort(int arr[], int n) {
-    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[i - 1]) {
+            int m = i;
+            while (m > 0 && arr[m] < arr[m-1]) {
+                swap(arr, m, m-1);
+                m--;
+            }
+        }
+    }
 }
 
 void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
     
+    int leftArr[n1], rightArr[n2];
+    
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArr[j] = arr[mid + 1 + j];
+    
+    int i = 0, j = 0, k = left;
+    
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+    //agrega lo que falte del arreglo 
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(int arr[], int left, int right) {
-    
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        
+        merge(arr, left, mid, right);
+    }
 }
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high) {//funcion auxiliar para la division de arreglos
+    int pivot = arr[high];
+    int i = low - 1;
     
-    return 0;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    
+    swap(arr, i + 1, high);
+    return i + 1;
 }
 
 void quickSort(int arr[], int low, int high) {
-    
+     if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+        
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
 }
 
 void heapify(int arr[], int n, int i) {
